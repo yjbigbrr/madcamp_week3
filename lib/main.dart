30eacaeb5863fee86dbd2b64b79db1ecd16d2fe0
yjbigbrr <youha11111@naver.com>
2024-07-15 14:59:12 +0900

    fdsafadsf

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'package:provider/provider.dart';
 import 'package:soccer_app/drawer/friend/friend_page.dart';
-import 'package:soccer_app/drawer/meeting/meeting_page.dart';
 import 'package:soccer_app/drawer/myplayer/myplayer_view_model.dart';
 import 'package:soccer_app/drawer/profile/profile_view_model.dart';
 import 'package:soccer_app/main_view_model.dart';
@@ -12,10 +9,7 @@ import 'package:soccer_app/login/login_screen.dart';
 import 'package:soccer_app/drawer/profile/profile_page.dart';
 import 'package:soccer_app/drawer/myplayer/myplayer_page.dart';
 import 'package:soccer_app/schedule/schedule_page.dart';
-import 'package:soccer_app/server/service/meetings_service.dart';
-import 'package:soccer_app/tab1/home_screen.dart';
-
-import 'drawer/meeting/meeting_view_model.dart'; // 추가
+import 'package:soccer_app/tab1/home_screen.dart'; // 추가
 
 void main() {
   KakaoSdk.init(nativeAppKey: '6cf381adbd9cf31b14c1db80c010a446');  // 실제 네이티브 앱 키로 대체하세요.
@@ -26,7 +20,6 @@ void main() {
         ChangeNotifierProvider(
           create: (context) => ProfileViewModel(),
         ),
-        ChangeNotifierProvider(create: (context) => MeetingViewModel(MeetingService(), Provider.of<ProfileViewModel>(context, listen: false))),
         ChangeNotifierProxyProvider<ProfileViewModel, MyPlayerViewModel>(
           create: (context) => MyPlayerViewModel(
             profileViewModel: Provider.of<ProfileViewModel>(context, listen: false),
@@ -138,9 +131,7 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
                   accountEmail: Text(profile?.id ?? 'User ID'),
                   currentAccountPicture: CircleAvatar(
                     backgroundColor: Colors.white,
-                    backgroundImage: profile?.profilePictureUrl.isNotEmpty ?? false
-                        ? FileImage(File(profile?.profilePictureUrl ?? ''))
-                        : AssetImage('assets/images/default_profile_picture.png') as ImageProvider,
+                    child: Text(profile?.nickname.substring(0, 1) ?? 'U'),
                   ),
                 ),
                 // Other Drawer items here
@@ -178,7 +169,7 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
                   onTap: () {
                     Navigator.push(
                       context,
-                      MaterialPageRoute(builder: (context) => MeetingPage()),
+                      MaterialPageRoute(builder: (context) => MyPlayerPage()),
                     );
                   },
                 ),
